48d8e8741a0aef196e7c34dbef09a266dc1f2338
Bennyboy2006 <1bennyboy2006@gmail.com>
2024-10-10 17:15:31 +0100

    Add Javadoc Comments

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,14 @@
 package com.napier.sem;
 
+/**
+ * This class is the main entry point for the program
+ */
 public class App {
+
+    /**
+     * This method prints hello world
+     * @param args - takes in arguments
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello World");
