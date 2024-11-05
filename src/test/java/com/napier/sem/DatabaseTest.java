9c476f4251918396d3f7eef7fa7dfefff77f37dd
Karol Droszkowski Raynor <raynorkarol@gmail.com>
2024-11-05 22:33:39 +0000

    Added test classes for every class

--- src/test/java/com/napier/sem/DatabaseTest.java
+++ src/test/java/com/napier/sem/DatabaseTest.java
@@ -2,14 +2,13 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.BooleanSupplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
+class DatabaseTest {
 
     @Test
-    public void validation() {
-
+    public void validateConnection() {
     }
 }
-
-
--- src/test/java/com/napier/sem/QueryTest.java
+++ src/test/java/com/napier/sem/QueryTest.java
@@ -4,12 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
+class QueryTest {
 
     @Test
-    public void validation() {
-
+    void runQuery() {
     }
 }
-
-
