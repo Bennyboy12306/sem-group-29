272223e3945924ff1536bcb9c24c79ab1f2390e8
Bennyboy2006 <1bennyboy2006@gmail.com>
2024-11-07 12:54:58 +0000

    First test with init

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -12,28 +12,20 @@ class AppTest
 
     // Note due to the nature of how our database works, test will only succeed if run from git actions
 
+    @BeforeAll
+    static void init()
+    {
+        Database.connect(true);
+    }
+
     /**
      * This test checks if the database has been connected successfully
      */
     @Test
     void databaseTest()
     {
-        Database.connect(true);
         assertNotNull(Database.getConnection());
     }
 
-    @Test
-    void queryTest()
-    {
-        try
-        {
-            var connection = Database.getConnection();
-            var statement = connection.prepareStatement("SHOW TABLES;");
-            var result = statement.executeQuery();
-            assertNotEquals( "", Query.formatResult(result, 1));
-        } catch (SQLException e)
-        {
-            assert false;
-        }
-    }
+
 }
