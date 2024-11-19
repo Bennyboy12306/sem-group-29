9572ca2a0dbcb0c0cd4695d34aef7595d00d33a7
Bennyboy2006 <1bennyboy2006@gmail.com>
2024-11-19 11:45:18 +0000

    Issue 14

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -9,6 +9,7 @@ public class App {
     // CONFIG OPTIONS
     private static final String ISSUE_12_NAME_OF_CONTINENT = "Europe";
     private static final String ISSUE_13_NAME_OF_REGION = "Eastern Europe";
+    private static final String ISSUE_14_N_COUNTRIES = "10";
     private static final String ISSUE_41_NAME_OF_CITY = "London";
     private static final String ISSUE_40_NAME_OF_DISTRICT = "Scotland";
     private static final String ISSUE_39_NAME_OF_COUNTRY = "United Kingdom";
@@ -42,6 +43,8 @@ public class App {
         Query.runQuery("SELECT name, population FROM country WHERE continent='" + ISSUE_12_NAME_OF_CONTINENT + "' ORDER BY population DESC;", 2, "12-As a user, I want to view all countries in a continent organized by largest population to smallest.", false);
 
         Query.runQuery("SELECT name, population FROM country WHERE region='" + ISSUE_13_NAME_OF_REGION + "' ORDER BY population DESC;", 2, "13-As a user, I want to view all countries in a region organized by largest population to smallest", false);
+
+        Query.runQuery("SELECT name, population FROM country ORDER BY population DESC LIMIT " + ISSUE_14_N_COUNTRIES + ";", 2, "14-As a user, I want to view the top N populated countries in the world where N is provided by me", false);
     }
 
 }
