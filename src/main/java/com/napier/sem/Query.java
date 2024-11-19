a6af3429a5b96ff2446ae15a4f43729428714f45
Karol Droszkowski Raynor <raynorkarol@gmail.com>
2024-11-19 15:50:33 +0000

    Query Null

--- src/main/java/com/napier/sem/Query.java
+++ src/main/java/com/napier/sem/Query.java
@@ -36,8 +36,7 @@ public class Query {
             if (result == null) {
                 System.out.println("Result is null, does not exist");
                 return resultString;
-
-        }
+            }
 
             while (result.next()) {
                 if (test)
