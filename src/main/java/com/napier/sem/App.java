c697fac9604821bceacea9224e0e53fb17b20f70
Bennyboy2006 <1bennyboy2006@gmail.com>
2024-11-20 11:44:21 +0000

    Issue 20

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -16,6 +16,7 @@ public class App {
     private static final String ISSUE_16_N_COUNTRIES = "4";
     private static final String ISSUE_18_NAME_OF_CONTINENT = "Asia";
     private static final String ISSUE_19_NAME_OF_REGION = "Eastern Europe";
+    private static final String ISSUE_20_NAME_OF_COUNTRY = "United Kingdom";
     private static final String ISSUE_41_NAME_OF_CITY = "London";
     private static final String ISSUE_40_NAME_OF_DISTRICT = "Scotland";
     private static final String ISSUE_39_NAME_OF_COUNTRY = "United Kingdom";
@@ -59,6 +60,8 @@ public class App {
         Query.runQuery("SELECT city.name, city.population FROM country JOIN city ON Code=CountryCode WHERE continent='" + ISSUE_18_NAME_OF_CONTINENT + "'ORDER BY population DESC;", 2, "18-As a user, I want to view all cities in a continent organised by largest population to smallest.", false);
 
         Query.runQuery("SELECT city.name, city.population FROM country JOIN city ON Code=CountryCode WHERE region='" + ISSUE_19_NAME_OF_REGION + "'ORDER BY population DESC;", 2, "19-As a user, I want to view all cities in a region organised by largest population to smallest.", false);
+
+        Query.runQuery("SELECT city.name, city.population FROM country JOIN city ON Code=CountryCode WHERE country.name='" + ISSUE_20_NAME_OF_COUNTRY + "'ORDER BY population DESC;", 2, "20-As a user, I want to view all cities in a country organised by largest population to smallest.", false);
     }
 
 }
